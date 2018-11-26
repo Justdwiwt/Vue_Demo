191516e8d411fc3b7cb126b1a4d233c569887a50
Justdwiwt <len_master@hotmail.com>
2018-11-26 21:22:46 +0800

    update environment

---
+++ src/components/Time.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{ date }}
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'Time'
+// }
+import Vue from 'vue'
+var app = new Vue({
+  el: '#app',
+  data: {
+    date: new Date()
+  },
+  mounted: function () {
+    var _this = this
+    this.timer = setInterval(function () {
+      _this.data = new Date()
+    }, 1000)
+  },
+  beforeDestroy: function () {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
